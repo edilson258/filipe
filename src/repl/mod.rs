739c9b40ba9f2889a6661008a997dd75b8775807
edilson258 <dev.258.edilson@gmail.com>
2024-05-06 17:29:28 +0200

    feat: improved REPL usability
    Updated helper, added support for multi-line editing

--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -4,33 +4,57 @@ use std::rc::Rc;
 use rustyline::error::ReadlineError;
 use rustyline::DefaultEditor;
 
+use crate::frontend::lexer::Lexer;
+use crate::frontend::parser::Parser;
 use crate::runtime::context::Context;
 use crate::runtime::flstdlib::builtins;
 use crate::runtime::object::Object;
 use crate::runtime::Runtime;
-use crate::frontend::lexer::Lexer;
-use crate::frontend::parser::Parser;
 
 const REPL_HELPER: &str = r#"
 Helper
     // define variable
-    let name: string;
-    let name = "Foo";
-    let name: string = "Foo";
+    // @Note: type annotation is optional 
+    //     when variables are initialized
+
+    let name = "Jonh Harvard"
+    let age: int = 85
+    let lovesCoffe = true
+    let height: float = 1.87
+
+    // Arrays
+
+    let xs: Array<int> = [1, 2, 3]
+    let ys = ["y1", "y2", "y3"]
 
     // define function
-    define sum(x: int, y: int): int { return x + y }
-    define sayHello(subject: string): null { print("Hello, ", subject) }
+
+    define sum(x: int, y: int): int {
+        let result = x + y
+        return result 
+    }
+
+    define sayHello(subject: string): void {
+        print("Hello, ", subject)
+    }
 
     // if-else statments
-    if true { // do something } else { // do something else }
+
+    if true { 
+        // do something 
+    } else { 
+        // do something else 
+    }
     
     // for loops
-    for counter in range(0, 10) { print(counter) }
+    for counter in range(0, 10) { 
+        print(counter) 
+    }
 
     // Built-in functions
-    len("Hello");
-    typeof(10);
+
+    len("Hello")
+    typeof(10)
 
     // More: ...
     // arthimetics: +, -, /, *
@@ -86,20 +110,53 @@ pub fn repl() {
     let env = Rc::new(RefCell::new(Context::from(builtins(), None)));
 
     loop {
-        let readline = rl.readline("|> ");
+        let readline = read_line(&mut rl, "|> ");
         match readline {
-            Ok(line) => {
-                let _ = rl.add_history_entry(line.as_str());
-                eval_repl_line(line, Rc::clone(&env));
-            }
-            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
-                println!("Exiting...");
-                break;
-            }
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break;
-            }
+            Some(line) => {
+                let mut state = String::new();
+                state.push_str(&line);
+                if !balance_and_eval(&mut rl, state, Rc::clone(&env)) {
+                    break;
+                }
+            },
+            None => break
         }
     }
+}
+
+fn balance_and_eval(rl: &mut DefaultEditor, mut state: String, env: Rc<RefCell<Context>>) -> bool {
+    loop {
+        if is_buf_balanced(&state) {
+            eval_repl_line(state.to_string(), env);
+            return true;
+        }
+        match read_line(rl, "...") {
+            Some(line) => state.push_str(&line),
+            None => return false,
+        }
+    }
+}
+
+fn read_line(rl: &mut DefaultEditor, prompt: &str) -> Option<String> {
+    let readline = rl.readline(prompt);
+    match readline {
+        Ok(line) => {
+            let _ = rl.add_history_entry(line.as_str());
+            Some(line)
+        }
+        Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
+            println!("Exiting...");
+            None
+        }
+        Err(err) => {
+            println!("Error: {:?}", err);
+            None
+        }
+    }
+}
+
+fn is_buf_balanced(buf: &str) -> bool {
+    let open_brace_count = buf.chars().filter(|c| *c == '{').count();
+    let close_brace_count = buf.chars().filter(|c| *c == '}').count();
+    open_brace_count <= close_brace_count
 }
