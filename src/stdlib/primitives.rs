5901795bc95a59513ead30136511d5cf1d66e96b
edilson258 <dev.258.edilson@gmail.com>
2024-05-15 20:22:16 +0200

    feat: support cast string to int

--- src/stdlib/primitives.rs
+++ src/stdlib/primitives.rs
@@ -21,8 +21,41 @@ impl<T> Primitive<T> {
 
 pub fn make_string(value: String) -> Primitive<String> {
     let mut fields: HashMap<String, Object> = HashMap::new();
-    fields.insert("length".to_string(), Object::Int(make_integer(value.len() as i64)));
+    fields.insert(
+        "length".to_string(),
+        Object::Int(make_integer(value.len() as i64)),
+    );
+    fields.insert(
+        "as_int".to_string(),
+        Object::BuiltInFunction(string_as_integer),
+    );
     Primitive::<String>::make(value, fields)
+}
+
+fn string_as_integer(args: Vec<ObjectInfo>) -> BuiltInFuncReturnValue {
+    if args.len() != 1 {
+        return BuiltInFuncReturnValue::Error(RuntimeError {
+            kind: ErrorKind::ArgumentError,
+            msg: format!("str.as_int takes 0 args but provided {}", args.len() - 1),
+        });
+    }
+
+    let str = match args[0].value.clone() {
+        Object::String(val) => val.value,
+        _ => "".to_string(),
+    };
+
+    let int = match str.parse::<i64>() {
+        Ok(val) => val,
+        Err(_) => {
+            return BuiltInFuncReturnValue::Error(RuntimeError {
+                kind: ErrorKind::ArgumentError,
+                msg: format!("Couldn't convert '{}' to int", str),
+            });
+        }
+    };
+
+    BuiltInFuncReturnValue::Object(Object::Int(make_integer(int)))
 }
 
 pub fn make_integer(value: i64) -> Primitive<i64> {
